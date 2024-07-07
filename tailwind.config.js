d8b74037ba7f17aaf63fc371014ed62e1062a559
qkrthdus605 <qkrthdus605@gmail.com>
2024-07-07 16:25:24 +0900

    style: font-face 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,30 @@
 module.exports = {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],  
-    theme: {
-      extend: {
-        colors: {
-          darkblue: '#001354',
-          skyblue: '#D4E8FF',
-        },
-        width: {
-          signupInput: '25rem',
-        },
-        maxWidth: {
-          'pc': '1440px',
-          'mobile': '767px',
-        },
-        fontFamily: {
-          'sans': ['MangoDdobak', 'Arial', 'sans-serif'],
-        },
-        fontSize: {
-          large: '46px',
-          regular: '24px'
-        }
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: '#001354',
+        skyblue: '#D4E8FF',
+      },
+      width: {
+        signupInput: '25rem',
+      },
+      maxWidth: {
+        pc: '1440px',
+        mobile: '767px',
+      },
+      fontFamily: {
+        sans: ['Arial', 'sans-serif'],
+        MangoBold: ['MangoBold'],
+        MangoRegular: ['MangoRegular'],
+        MangoLight: ['MangoLight'],
+      },
+      fontSize: {
+        large: '46px',
+        regular: '24px',
+        small: '18px',
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
