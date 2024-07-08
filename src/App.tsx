b80d4b779ca7745a8f95ea36dc718b34de5ebff3
qkrthdus605 <qkrthdus605@gmail.com>
2024-07-08 10:16:19 +0900

    setting: 패키지 및 tailwind 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home/Home';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Blog</h1>
-      <h3>로그인 안했을 때 로그인 보여주기</h3>
+      <Home />
     </QueryClientProvider>
   );
 }
