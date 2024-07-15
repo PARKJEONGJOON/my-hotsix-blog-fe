24d63ef7a71e8a4ba9ee02b3fb9f24b8124faa4c
PARKJEONGJOON <99483101+PARKJEONGJOON@users.noreply.github.com>
2024-07-15 18:20:42 +0900

    fix: type error

--- src/types/UserData.ts
+++ src/types/UserData.ts
@@ -1,8 +1,8 @@
 export interface UserData {
-  userId: string;
-  password: string;
-  userName: string;
-  profileImg: string | null;
+  userId?: string;
+  password?: string;
+  userName?: string;
+  profileImg?: string | null;
   gitUrl?: string | undefined;
   introduce?: string | undefined;
 }
