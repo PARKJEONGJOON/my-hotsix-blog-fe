8590940fa8b1eeb6914b9ca48f59951015699aaf
Kim Wongi <woongi3012@gmail.com>
2024-07-17 21:43:42 +0900

    chore : 더미데이터 관련

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -13,22 +13,34 @@ export interface RegisterData {
   introduce?: string;
 }
 
+export interface sideUserData {
+  name: string;
+  email: string;
+  github?: string;
+  introduce?: string;
+}
+
 export const login = async (data: LoginData) => {
-  const response = await axiosInstance.post('/users/login', data);
+  const response = await axiosInstance.post('/api/users/login', data);
   return response.data;
 };
 
 export const register = async (data: RegisterData) => {
-  const response = await axiosInstance.post('/users', data);
+  const response = await axiosInstance.post('/api/users', data);
   return response.data;
 };
 
 export const logout = async () => {
   try {
-    await axiosInstance.post('/users/logout');
+    await axiosInstance.post('/api/users/logout');
     return true;
   } catch (error) {
     console.log('로그아웃 실패', error);
     return false;
   }
 }
+
+export const sideUser = async () => {
+  const response = await axiosInstance.get('/api/users');
+  return response.data;
+};
--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', // JSON Server가 동작 중인 주소
+  baseURL: 'http://localhost:5000', // JSON Server가 동작 중인 주소
   timeout: 10000, // 요청 시간 초과 설정
   withCredentials: true, // 쿠키 사용을 위한 설정 (옵션)
   headers: {
